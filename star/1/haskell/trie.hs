3cb0a9606ecd152f1b63d0ccceeb04cdbabb3ce8
dayvidpham <dayvidpham@gmail.com>
2023-12-03 03:45:32 -0800

    Added addWord method

--- star/1/haskell/trie.hs
+++ star/1/haskell/trie.hs
@@ -2,18 +2,22 @@ data TrieNode = TrieNode {
     dict :: [(Char, TrieNode)]
 } deriving (Eq, Show)
     
-addWord :: String -> TrieNode -> TrieNode -> TrieNode
-addWord str curr root
+addWord :: String -> TrieNode -> TrieNode
+addWord str root
     | null str = 
         root
     | null found =
-        let 
-            updated = TrieNode $ (key, TrieNode []) : (dict root) 
-            next    = snd $ head (dict updated)
-        in
-            addWord suffix next updated
+        TrieNode $ (key, addWord suffix (TrieNode [])) : (dict root) 
     | otherwise =
-        addWord suffix (snd $ head found) root
+        let
+            (sx, post) = break ((key ==) . fst) (dict root)
+            (x, xs) = if length post > 1
+                then (head post, tail post)
+                else (head post, [])
+
+            rec = addWord suffix (snd x)
+            y = (key, rec)
+        in TrieNode $ concat [sx, [y], xs]
     where
         (key : suffix) = str
         found = filter (\item -> (fst item) == key) (dict root)
@@ -29,10 +33,13 @@ nest str = case str of
     [x]         -> (x, Node '\n' ListEnd)
     (x:x2:xs)   -> (x, Node x2 $ snd $ nest (x2:xs))
 
+main :: IO ()
+main =
+    let
+        a = addWord "hello" (TrieNode [])
+        b = addWord "heart" a
+    in putStrLn $ show $ b
+
 -- main :: IO ()
 -- main = 
---     putStrLn $ show $ addWord "hel" (TrieNode []) (TrieNode [])
-
-main :: IO ()
-main = 
-    putStrLn $ show $ nest "hello"
+--     putStrLn $ show $ nest "hello"
