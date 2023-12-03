ab2c435425c25cf65c7b4dcf07040f7da3a95628
dayvidpham <dayvidpham@gmail.com>
2023-12-03 05:33:56 -0800

    Add end marker for addWord

--- star/1/haskell/trie.hs
+++ star/1/haskell/trie.hs
@@ -1,22 +1,26 @@
 data TrieNode = TrieNode {
     dict :: [(Char, TrieNode)]
 } deriving (Eq, Show)
-    
-addWord :: String -> TrieNode -> TrieNode
-addWord str root
+addWord' :: String -> TrieNode -> TrieNode
+addWord' str root
     | null str = 
         root
     | null found =
-        TrieNode $ (key, addWord suffix (TrieNode [])) : (dict root) 
+        TrieNode $ (key, addWord' suffix (TrieNode [])) : (dict root) 
     | otherwise =
         let
             (sx, (x:xs)) = break ((key ==) . fst) (dict root)
-            rec = addWord suffix (snd x)
+            rec = addWord' suffix (snd x)
             y = (key, rec)
         in TrieNode $ concat [sx, [y], xs]
     where
         (key : suffix) = str
         found = filter (\item -> (fst item) == key) (dict root)
+
+
+addWord :: String -> TrieNode -> TrieNode
+addWord str root = addWord' (str ++ "\n") root
+
 
 data Node k = Node {
     val :: k,
@@ -29,12 +33,22 @@ nest str = case str of
     [x]         -> (x, Node '\n' ListEnd)
     (x:x2:xs)   -> (x, Node x2 $ snd $ nest (x2:xs))
 
+
 main :: IO ()
 main =
     let
-        a = addWord "hello" (TrieNode [])
-        b = addWord "heart" a
-    in putStrLn $ show $ b
+        vocab = [ 
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
+        root = foldr (\str -> \trie -> addWord str trie) (TrieNode []) vocab
+    in putStrLn $ show root
+
+--main :: IO ()
+--main =
+--    let
+--        a = addWord "hello" (TrieNode [])
+--        b = addWord "heart" a
+--    in putStrLn $ show $ b
 
 -- main :: IO ()
 -- main = 
