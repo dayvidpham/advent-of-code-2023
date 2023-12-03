386ed59e2435c3377d8c4535894d42a2dd03dff1
dayvidpham <dayvidpham@gmail.com>
2023-12-02 21:56:11 -0800

    Add singly linked list

--- star/1/haskell/trie.hs
+++ star/1/haskell/trie.hs
@@ -1,6 +1,38 @@
-data TrieValue = TrieValue String [(String, TrieValue)]
+data TrieNode = TrieNode {
+    dict :: [(Char, TrieNode)]
+} deriving (Eq, Show)
+    
+addWord :: String -> TrieNode -> TrieNode -> TrieNode
+addWord str curr root
+    | null str = 
+        root
+    | null found =
+        let 
+            updated = TrieNode $ (key, TrieNode []) : (dict root) 
+            next    = snd $ head (dict updated)
+        in
+            addWord suffix next updated
+    | otherwise =
+        addWord suffix (snd $ head found) root
+    where
+        (key : suffix) = str
+        found = filter (\item -> (fst item) == key) (dict root)
+
+data Node k = Node {
+    val :: k,
+    nxt :: Node k
+} | ListEnd deriving (Eq, Show)
+
+nest :: String -> (Char, Node Char)
+nest str = case str of
+    []          -> ('\n', ListEnd)
+    [x]         -> (x, Node '\n' ListEnd)
+    (x:x2:xs)   -> (x, Node x2 $ snd $ nest (x2:xs))
+
+-- main :: IO ()
+-- main = 
+--     putStrLn $ show $ addWord "hel" (TrieNode []) (TrieNode [])
 
 main :: IO ()
-main = return ()
-
--- addWord :: String -> [(String, [(String, String])] -> [(String, [String])]
+main = 
+    putStrLn $ show $ nest "hello"
