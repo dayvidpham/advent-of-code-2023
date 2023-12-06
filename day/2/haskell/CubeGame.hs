2392ebcef8995463eb2e023b60a0c64a57c1f9ad
dayvidpham <dayvidpham@gmail.com>
2023-12-06 03:48:35 -0800

    Add dict data type

--- day/2/haskell/CubeGame.hs
+++ day/2/haskell/CubeGame.hs
@@ -2,11 +2,36 @@ module CubeGame where
 
 import System.IO
 
+data Dict k v = Dict {
+    dItems :: [(k, v)]
+} deriving (Ord, Eq, Show)
+
+dGetPivot :: Ord k => k -> Dict k v -> ( [(k, v)], [(k, v)], [(k, v)] )
+dGetPivot key dict = case pivots of
+    (pre, pivot) -> (pre, [match], post)
+        where 
+            (match:post) = pivot
+    where 
+        isKey = (\x -> key == (fst x))
+        pivots = break isKey $ dItems dict 
+
+dGetItem :: Ord k => k -> Dict k v -> Maybe (k, v)
+dGetItem key dict = case found of
+    []      -> Nothing
+    (kv:_)  -> Just kv
+    where found = filter (\itm -> key == (fst itm)) $ dItems dict
+
+dPut :: Ord k => k -> v -> Dict k v -> Dict k v
+dPut key val dict = case entry of
+    []  -> Dict $ [(key, val)] ++ dItems dict
+    _   -> Dict $ concat [pre, [(key, val)], post]
+    where (pre, entry, post) = dGetPivot key dict
+
 splitStr :: String -> String -> [String]
 splitStr str seps = case dropWhile isSep str of
                         "" -> []
                         s' -> w : splitStr s'' seps
-                            where (w, s'') = break isSep s'
+                              where (w, s'') = break isSep s'
                         where isSep = (\c -> elem c seps)
 
 printAllLines :: Handle -> IO ()
@@ -15,10 +40,11 @@ printAllLines fp = do
     case isEOF of   True  -> return ()
                     False -> do
                         ln      <- hGetLine fp
-                        unGame  <- return $ drop 5 ln
-                        splits  <- return $ splitStr unGame ",;: "
+                        splits  <- return $ splitStr ln ",;:"
                         pairs   <- return $ map words splits
-                        putStrLn . show $ pairs
+                        (id:vkColours) <- return $ pairs
+                        kvColours <- return $ map reverse vkColours
+                        putStrLn . show $ kvColours
                         printAllLines fp
 
 main :: IO ()
