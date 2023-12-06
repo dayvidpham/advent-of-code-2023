6361a8448a1c9b0a22cd1f3af4be8a79abdeb700
dayvidpham <dayvidpham@gmail.com>
2023-12-05 22:23:42 -0800

    Add string splitting

--- day/2/haskell/CubeGame.hs
+++ day/2/haskell/CubeGame.hs
@@ -3,15 +3,22 @@ module CubeGame where
 import System.IO
 
 splitStr :: String -> String -> [String]
-splitStr str sep = [str]
+splitStr str seps = case dropWhile isSep str of
+                        "" -> []
+                        s' -> w : splitStr s'' seps
+                            where (w, s'') = break isSep s'
+                        where isSep = (\c -> elem c seps)
 
 printAllLines :: Handle -> IO ()
 printAllLines fp = do 
     isEOF <- hIsEOF fp
     case isEOF of   True  -> return ()
                     False -> do
-                        ln <- hGetLine fp
-                        putStrLn . show $ splitStr ln ""
+                        ln      <- hGetLine fp
+                        unGame  <- return $ drop 5 ln
+                        splits  <- return $ splitStr unGame ",;: "
+                        pairs   <- return $ map words splits
+                        putStrLn . show $ pairs
                         printAllLines fp
 
 main :: IO ()
