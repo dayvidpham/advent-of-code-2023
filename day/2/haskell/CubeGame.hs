6bd9347c3fa3fa1d9f9dc45410a1832c0de4c478
dayvidpham <dayvidpham@gmail.com>
2023-12-06 18:39:39 -0800

    Partition items into list of (clr, [values])

--- day/2/haskell/CubeGame.hs
+++ day/2/haskell/CubeGame.hs
@@ -15,17 +15,23 @@ dGetPivot key dict = case pivots of
         isKey = (\x -> key == (fst x))
         pivots = break isKey $ dItems dict 
 
+dPut :: Ord k => k -> v -> Dict k v -> Dict k v
+dPut key val dict = case entry of
+    []  -> Dict $ [(key, val)] ++ dItems dict
+    _   -> Dict $ concat [pre, [(key, val)], post]
+    where (pre, entry, post) = dGetPivot key dict
+
 dGetItem :: Ord k => k -> Dict k v -> Maybe (k, v)
 dGetItem key dict = case found of
     []      -> Nothing
     (kv:_)  -> Just kv
     where found = filter (\itm -> key == (fst itm)) $ dItems dict
 
-dPut :: Ord k => k -> v -> Dict k v -> Dict k v
-dPut key val dict = case entry of
-    []  -> Dict $ [(key, val)] ++ dItems dict
-    _   -> Dict $ concat [pre, [(key, val)], post]
-    where (pre, entry, post) = dGetPivot key dict
+dGet :: Ord k => k -> Dict k v -> Maybe v
+dGet key dict = case found of
+    Nothing  -> Nothing
+    Just kv  -> Just $ snd kv
+    where found = dGetItem key dict
 
 splitStr :: String -> String -> [String]
 splitStr str seps = case dropWhile isSep str of
@@ -34,23 +40,36 @@ splitStr str seps = case dropWhile isSep str of
                               where (w, s'') = break isSep s'
                         where isSep = (\c -> elem c seps)
 
-printAllLines :: Handle -> IO ()
-printAllLines fp = do 
+partitionByKeys :: Ord k => [k] -> [(k,v)] -> [ (k,[v]) ]
+partitionByKeys keys items = foldl partition [] keys
+    where filterItems key   = filter ((== key) . fst) items
+          partition acc key = acc ++ [(key, map (snd) $ filterItems key)]
+
+printAllLines :: Integer -> Dict String Integer -> Handle -> IO ()
+printAllLines acc maxs fp = do 
     isEOF <- hIsEOF fp
-    case isEOF of   True  -> return ()
-                    False -> do
-                        ln      <- hGetLine fp
-                        splits  <- return $ splitStr ln ",;:"
-                        pairs   <- return $ map words splits
-                        (id:vkColours) <- return $ pairs
-                        kvColours <- return $ map reverse vkColours
-                        putStrLn . show $ kvColours
-                        printAllLines fp
+    case isEOF of   
+        True  -> return ()
+        False -> do
+            ln <- hGetLine fp
+            let splits              = splitStr ln ",;:"
+            let (idList:colourList) = map words splits
+            let (idK:idV:_)         = idList 
+            let id                  = (idK, read idV :: Integer)
+            let colourItems        = map (\vk -> let (v:k:_) = vk in (k, read v :: Integer)) colourList
+            let select              = partitionByKeys clrs colourItems
+            --let items               = concat [[id], colours]
+            --putStrLn . show $ items
+            
+            putStrLn . show $ select
+            printAllLines acc maxs fp
+            where clrs = ["red", "green", "blue"]
 
 main :: IO ()
 main =
     let
         inputPath = "../2-input.txt";
+        maxs = Dict [("red", 12), ("green", 13), ("blue", 14)]
     in do
-        withFile "../2-input.txt" ReadMode printAllLines
+        withFile "../2-input.txt" ReadMode (printAllLines 0 maxs)
 
