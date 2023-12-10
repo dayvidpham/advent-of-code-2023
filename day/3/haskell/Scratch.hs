4f60eb34e34156f28138119bf6cdd47903bb96b4
dayvidpham <dayvidpham@gmail.com>
2023-12-10 15:15:03 -0800

    Remove unecessary parts from previous day

--- day/3/haskell/Scratch.hs
+++ day/3/haskell/Scratch.hs
@@ -1,82 +1,6 @@
 module Scratch where
 
 import System.IO
-import Control.Arrow
-
-data Dict k v = Dict {
-    dItems :: [(k, v)]
-} deriving (Ord, Eq, Show)
-
-dGetPivot :: Ord k => k -> Dict k v -> ( [(k, v)], [(k, v)], [(k, v)] )
-dGetPivot key dict = case pivots of
-    (pre, pivot) -> (pre, [match], post)
-        where 
-            (match:post) = pivot
-    where 
-        isKey = (\x -> key == (fst x))
-        pivots = break isKey $ dItems dict 
-
-dPut :: Ord k => k -> v -> Dict k v -> Dict k v
-dPut key val dict = case entry of
-    []  -> Dict $ [(key, val)] ++ dItems dict
-    _   -> Dict $ concat [pre, [(key, val)], post]
-    where (pre, entry, post) = dGetPivot key dict
-
-dGetItem :: Ord k => k -> Dict k v -> Maybe (k, v)
-dGetItem key dict = case found of
-    []      -> Nothing
-    (kv:_)  -> Just kv
-    where found = filter (\itm -> key == (fst itm)) $ dItems dict
-
-dGet :: Ord k => k -> Dict k v -> Maybe v
-dGet key dict = case found of
-    Nothing  -> Nothing
-    Just kv  -> Just $ snd kv
-    where found = dGetItem key dict
-
-splitStr :: String -> String -> [String]
-splitStr str seps = case dropWhile isSep str of
-                        "" -> []
-                        s' -> w : splitStr s'' seps
-                              where (w, s'') = break isSep s'
-                        where isSep = (\c -> elem c seps)
-
-partitionByKeys :: Ord k => [k] -> [(k,v)] -> [ (k,[v]) ]
-partitionByKeys keys items = foldl partition [] keys
-    where filterItems key   = filter ((== key) . fst) items
-          partition acc key = acc ++ [(key, map (snd) $ filterItems key)]
-
-printAllLines :: Integer -> Handle -> IO Integer
-printAllLines acc fp = do 
-    isEOF <- hIsEOF fp
-    case isEOF of   
-        True  -> return acc
-        False -> do
-            ln <- hGetLine fp
-            let splits              = splitStr ln ",;:"
-            let (idList:colourList) = map words splits
-            let (_:idVStr:_)        = idList 
-            let idV                 = read idVStr :: Integer
-            let colourItems         = map revListToItem colourList
-            let dict                = Dict $ partitionByKeys clrs colourItems
-            let maxCnts             = fmap (\clr -> clrToMaxCnt clr dict) clrs
-            let power               = foldl (*) 1 maxCnts
-            printAllLines (acc+power) fp
-            where clrs = ["red", "green", "blue"]
-                  revListToItem vk  = let (v:k:_) = vk in (k, read v :: Integer)
-                  clrToMaxCnt clr ddict = case mns of
-                    Nothing -> -1
-                    Just ns -> foldl max 0 ns
-                    where mns   = dGet clr ddict
-
----------------------------------------------------------------------------------------------------------
-
-{-
-lineToDict :: (Integer -> Integer -> Char -> Dict) -> Integer -> String -> Dict
-lineToDict fPutItem col ln = case dropWhile isDot ln of
-    ""  -> ,
-    where isDot c = c == '.'
--}
 
 isNumber :: Char -> Bool
 isNumber c = c `elem` "0123456789"
@@ -93,14 +17,6 @@ hasSymbol ln = case dropWhile notSymbol ln of
 mapTuple3 :: (a -> b) -> (a, a, a) -> (b, b, b)
 mapTuple3 f (x, y, z) = (f x, f y, f z)
 
-
-{-
-lineToParts :: (String, String, String) -> [Integer] -> [Integer] 
-lineToParts 
-  (_, "", _) 
-  parts = 
-    reverse parts
--}
 
 lineToParts :: (String, String, String) -> [Integer] -> IO [Integer] 
 lineToParts 
