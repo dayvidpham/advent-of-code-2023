064c61f0048715fc679f78dff653b5fdd2455438
dayvidpham <dayvidpham@gmail.com>
2023-12-11 21:12:33 -0800

    Add splitting lines to correct parts

--- day/4/haskell/Brute.hs
+++ day/4/haskell/Brute.hs
@@ -1,6 +1,8 @@
 module Brute where
 
 import System.IO
+import Control.Arrow ((***))
+import Control.Monad (join)
 
 isNumber :: Char -> Bool
 isNumber c = c `elem` "0123456789"
@@ -24,7 +26,31 @@ indexOf y xs idx = case xs of
 ----------------------------------------------------------
 -- Brutish: no use of standard lib or Data.Set
 
+data Game = Game {
+    gameId      :: Int,
+    gameWins    :: [Int],
+    gameDraws   :: [Int]
+} deriving (Ord, Eq, Show)
 
+mkParts :: [String] -> [(String, [String], [String])]
+mkParts lns = map (splitParts . mkFlatTup . mkNum_Parts . mkGame_Nums) lns
+    where mkGame_Nums ln                    = break (\ c -> c == ':') ln
+          mkNum_Parts (game, nums)          = (game, break (\ c -> c == '|') $ drop 2 nums)
+          mkFlatTup (game, (wins, draws))   = (game, wins, drop 2 draws)
+          splitParts (game, wins, draws)    = (game, words wins, words draws)
+          readParts (game, wins, draws)     = (game, winNums, drawsNums)
+            where winNums = read wins :: [Int]
+                  drawsNums = read draws :: [Int]
+
+{-
+mkParts :: [String] -> [(String, [String], [String])]
+mkParts lns = map (readParts . splitParts . mkFlatTup . mkNum_Parts . mkGame_Nums) lns
+    where mkGame_Nums ln                    = break (\ c -> c == ':') ln
+          mkNum_Parts (game, nums)          = (game, break (\ c -> c == '|') $ drop 2 nums)
+          mkFlatTup (game, (wins, draws))   = (game, wins, drop 2 draws)
+          splitParts (game, wins, draws)    = (game, words wins, words draws)
+          readParts (game, wins, draws)     = (game, (read wins :: [Int]), (read draws :: [Int]))
+-}
 
 main :: IO ()
 main =
@@ -32,5 +58,10 @@ main =
         inputPath   = "../4-input.txt";
     in do
         inputText   <- readFile inputPath
-        let lns = lines inputText
-        print lns
+        let lns     = lines inputText
+            parts   = mkParts lns
+            (gameStr, winsStr, drawsStr) = unzip3 $ mkParts lns
+            --(wins, draws) = (join (***)) (map words) (winsStr, drawsStr)
+        putStrLn $ foldl (\ acm part -> ("\n" ++ show part ++ acm)) "" (reverse parts)
+        --print draws
+
